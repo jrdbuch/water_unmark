46581b535d9dbdd04904fac34f59cf04adc1fd85
Jared Buchanan <buchanan@braincorporation.com>
2019-03-28 17:36:08 -0700

    continued reorganizing code into class structure

--- estimate_W.py
+++ estimate_W.py
@@ -18,9 +18,17 @@ class WaterUnmark():
         self._imgs_boundary = []
         self,_imgs_edges = []
 
+        #replace this with numpy func
+        self._elementwise_2Dmag_fn = lambda n, Gx, Gy: np.vectorize(lambda x, y: (x ** 2 + y ** 2) ** 0.5)(Gx[:, :, n],
+                                                                                                     Gy[:, :,
+                                                                                                     n])  # element wise magnitude fn
+
+        self._edge_detect_fn = lambda n, sigma, thresh_frac, img: feature.canny(img[:, :, n], sigma=sigma,
+                                                                          high_threshold=thresh_frac * np.max(img))
+
     def import_training_images(self, path_to_dir):
         img_paths = os.listdir(path_to_dir)
-        self.imgs_raw = []
+        self._imgs_raw = []
 
         for img_path in img_paths:
             self._imgs_raw.append(io.imread(path_to_dir + '\\' + img_path))
@@ -44,6 +52,7 @@ class WaterUnmark():
     @staticmethod
     def _find_pixelwise_median(img_list):
         #finds the pixelwise median over a collection of imgs
+        #TO DO: Replace with numpy function
 
         img_median = np.zeros(img_list[0].shape)
 
@@ -54,73 +63,95 @@ class WaterUnmark():
 
         return img_median
 
-    def _find_elementwise_
+    def _extract_img_boundaries(self):
+        #TO DO: make this robust for shifted watermarks
+        # gather image boundary to be used for later poisson reconstruction
+        img_boundary = np.zeros(img.shape)  # new img with only the boundaries of the orginal img
+        img_boundary[0, :, :] = img[0, :, :]
+        img_boundary[-1, :, :] = img[-1, :, :]
+        img_boundary[:, 0, :] = img[:, 0, :]
+        img_boundary[:, -1, :] = img[:, -1, :]
 
+        return img_boundary
+
+    def _preprocess_img (self, img):
+        # preprocess raw image
+        filter_img_fn = lambda n, sigma, img: ndi.gaussian_filter(img[:, :, n], sigma=sigma)
+        img_processed = self._apply_fn_to_all_colorchannels(filter_img_fn, 3, 1, img)  # run filter over img
+
+        return img_processed
+
+
+    def _extract_img_gradients(self, img):
+        # find img gradients x,y, and magnitude using sobel filter
+        img_Gx = ndi.sobel(img, axis=0)
+        img_Gy = ndi.sobel(img, axis=1)
+
+
+        img_Gmag = self._apply_fn_to_all_colorchannels(self._elementwise_2Dmag_fn, 3, img_Gx, img_Gy)
+
+        return img_Gx, img_Gy, img_Gmag
+
+    def _extract_edgemap(self, img):
+        # find verbose edge map
+
+        img_edges = self._apply_fn_to_all_colorchannels(edge_detect_fn, 3, 1, 0.4, img_Gmag)
+
+        return img_edges
+
+    def reconstruct_watermark(self, img_Gx, img_Gy, img_boundary):
+        reconstruct_w_fn = lambda n, Gx, Gy, boundary: poisson_reconstruct(Gy[:, :, n], Gx[:, :, n], boundary[:, :, n])
+        w_estimate = self._apply_fn_to_all_colorchannels(reconstruct_w_fn, 3, img_Gx, img_Gy, img_boundary)
+
+        return w_estimate
+
+
+    def _find_elementwise_(self):
+        pass
 
     def estimate_watermark(self):
 
         for img in self._raw_imgs:
-            # process raw image
-            filter_img_fn = lambda n, sigma, img: ndi.gaussian_filter(img[:, :, n], sigma=sigma)
-            img_processed = apply_fn_to_all_colorchannels(filter_img_fn, 3, 1, img)  # run filter over img
 
-            # find img gradients x,y, and magnitude using sobel filter
-            img_Gx = ndi.sobel(img_processed, axis=0)
-            img_Gy = ndi.sobel(img_processed, axis=1)
+            img_processed = self._preprocess_img(img)
+            img_Gx, img_Gy, img_Gmag =  self._extract_img_gradients(img_processed)
+            img_edges = self._extract_edgemap(img)
+            img_boundary = self._extract_img_gradients(self, img)
 
-            elementwise_2Dmag_fn = lambda n, Gx, Gy: np.vectorize(lambda x, y: (x ** 2 + y ** 2) ** 0.5)(Gx[:, :, n],
-                                                                                                         Gy[:, :,
-                                                                                                         n])  # element wise magnitude fn
-            img_Gmag = apply_fn_to_all_colorchannels(elementwise_2Dmag_fn, 3, img_Gx, img_Gy)
-
-            # find verbose edge map
-            edge_detect_fn = lambda n, sigma, thresh_frac, img: feature.canny(img[:, :, n], sigma=sigma,
-                                                                              high_threshold=thresh_frac * np.max(img))
-            img_edges = apply_fn_to_all_colorchannels(edge_detect_fn, 3, 1, 0.4, img_Gmag)
-
-            # gather image boundary to be used for later poisson reconstruction
-            img_boundary = np.zeros(img.shape)  # new img with only the boundaries of the orginal img
-            img_boundary[0, :, :] = img[0, :, :]
-            img_boundary[-1, :, :] = img[-1, :, :]
-            img_boundary[:, 0, :] = img[:, 0, :]
-            img_boundary[:, -1, :] = img[:, -1, :]
 
             # store single data sample in list
-            imgs_processed.append(img_processed)
-            imgs_boundary.append(img_boundary)
-            imgs_Gx.append(img_Gx)
-            imgs_Gy.append(img_Gy)
-            imgs_Gmag.append(img_Gmag)
-            imgs_edges.append(img_edges)
+            self._imgs_processed.append(img_processed)
+            self._imgs_boundary.append(img_boundary)
+            self._imgs_Gx.append(img_Gx)
+            self._imgs_Gy.append(img_Gy)
+            self._imgs_Gmag.append(img_Gmag)
+            self._imgs_edges.append(img_edges)
 
         for _ in range(1):
             ### Watermark estimation###
 
             # find pixelwise gradient medians over all images
-            img_Gx_median = find_pixelwise_median(imgs_Gx)
-            img_Gy_median = find_pixelwise_median(imgs_Gy)
-            img_Gmag_median = apply_fn_to_all_colorchannels(elementwise_2Dmag_fn, 3, img_Gx_median, img_Gy_median)
+            img_Gx_median = self._find_pixelwise_median(self._imgs_Gx)
+            img_Gy_median = self._find_pixelwise_median(self._imgs_Gy)
+            img_Gmag_median = self._apply_fn_to_all_colorchannels(self._elementwise_2Dmag_fn, 3, self._img_Gx_median, self._img_Gy_median)
 
             # find median img boundary
-            img_boundary_median = find_pixelwise_median(imgs_boundary)
+            img_boundary_median = self._find_pixelwise_median(self._imgs_boundary)
 
             # extract edges
-            img_edges_median = apply_fn_to_all_colorchannels(edge_detect_fn, 3, 1, 0.3, img_Gmag_median).astype(
-                bool)  # run canny edge detection
-            bounding_box = ndi.find_objects(img_edges_median)[0]  # find bounding box around objects where edges = True
+            img_edges_median = self._apply_fn_to_all_colorchannels(self._edge_detect_fn, 3, 1, 0.3, img_Gmag_median).astype(bool)  # run canny edge detection
+            #bounding_box = ndi.find_objects(img_edges_median)[0]  # find bounding box around objects where edges = True
 
             # use edges to locate the gradients of interest for poisson reconstruction
-            img_Gx_reconstruct = np.multiply(img_Gx_median, img_edges_median)
-            img_Gy_reconstruct = np.multiply(img_Gy_median, img_edges_median)
+            img_Gx_reconstruct = np.multiply(self._img_Gx_median, self._img_edges_median)
+            img_Gy_reconstruct = np.multiply(self._img_Gy_median, self._img_edges_median)
 
             ###Watermark detection### not needed if bounding box for the template is the entire image
-            # match = cv2.matchTemplate(img_edges_median[:,:,0].astype(np.uint8),imgs_edges[0][:,:,0].astype(np.uint8),method=eval('cv2.TM_CCOEFF')) #template, img
+            #match = cv2.matchTemplate(img_edges_median[:,:,0].astype(np.uint8),imgs_edges[0][:,:,0].astype(np.uint8),method=eval('cv2.TM_CCOEFF')) #template, img
 
         # poisson reconstruction
-        reconstruct_w_fn = lambda n, Gx, Gy, boundary: poisson_reconstruct(Gy[:, :, n], Gx[:, :, n], boundary[:, :, n])
-        w_estimate = apply_fn_to_all_colorchannels(reconstruct_w_fn, 3, img_Gx_reconstruct, img_Gy_reconstruct,
-                                                   img_boundary_median)
 
+        w_estimate = self._reconstruct_watermark(img_Gx_reconstruct, img_Gy_reconstruct, img_boundary_median)
 
 
 
@@ -131,21 +162,6 @@ def resize_imgs(img_list,mode='centered_crop'):
     min_y = min([img.shape[1] for img in img_list])
 
 
-
-
-
-
-#main
-
-#import all images to list
-img_dir_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__), r"examples/123RF")
-imgs = import_images(img_dir_filepath)
-
-#storage vars where list index = image index
-imgs_processed = []
-imgs_Gx = []
-imgs_Gy = []
-imgs_Gmag = []
-imgs_boundary = []
-imgs_edges = []
+if __name__ == "__main__":
+    pass
 
